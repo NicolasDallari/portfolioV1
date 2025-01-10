d20c4a897dbd49beea620fc78c7fe117f8e29f8c
dallari nicolas <nicogamer@hotmail.fr>
2025-01-10 15:33:36 +0100

    commit fait avant de rectifier les problemes rencontrer une foi le site mis en prod

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '8ab06c64e8cca835677f14f5f09e3e577f1067ca',
+        'reference' => 'f123615e7098acfd798ae11c0978418082c1b824',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '8ab06c64e8cca835677f14f5f09e3e577f1067ca',
+            'reference' => 'f123615e7098acfd798ae11c0978418082c1b824',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
